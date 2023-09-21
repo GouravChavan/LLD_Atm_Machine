ce26850a7779e8eaf76ae57c6ef0612d8d2382d2
GouravChavan <75515239+GouravChavan@users.noreply.github.com>
2023-09-21 13:01:37 +0530

    Update atm_machine.py
    Used encapsulation for data hiding Made instance variables private.

--- atm_machine.py
+++ atm_machine.py
@@ -1,37 +1,37 @@
 class Atm:
     def __init__(self):
-        self.pin =''
-        self.balance = 0
-        self.menu()
+        self.__pin =''
+        self.__balance = 0
+        self.__menu()
         
         
     def menu(self):
         user_input = input("""
         How would you like to proceed?
-        1.To crete pin
-        2.Deposit
-        3.Withdraw
-        4.Balance
-        5.Exit
+        1. To create a pin
+        2. Deposit
+        3. Withdraw
+        4. Balance
+        5. Exit
 """)
         
         if user_input == '1':
             self.create_pin()       #For Creating pin
-            self.menu()             #For repetition
+            self.__menu()             #For repetition
         elif user_input == '2':
             self.deposit()          #For Deposit
-            self.menu()
+            self.__menu()
         elif user_input == '3':     
             self.withdraw()         #For witdraw
-            self.menu()
+            self.__menu()
         elif user_input == '4':     
             self.check_balance()    #For checking balance
-            self.menu()
+            self.__menu()
         elif user_input == '5':
             print("Bye")            #For Exit
         else:
             print("Invalid")
-            self.menu()
+            self.__menu()
     
     def create_pin(self):
         self.pin = input("Enter your pin to create :")
@@ -39,33 +39,32 @@ class Atm:
     
     def deposit(self):
         temp = input("Enter your pin :")
-        if temp == self.pin:
+        if temp == self.__pin:
             amount = int(input("Enter amount to be deposited:"))
-            self.balance += amount
+            self.__balance += amount
             print("Deposited successfully")
         else:
             print("Invalid Pin")
     def withdraw(self):
         temp = input("Enter your pin :")
-        if temp == self.pin:
+        if temp == self.__pin:
             amount = int(input("Enter amount to be Withdrawan:"))
-            if amount <= self.balance:
-                self.balance -= amount
-                print("Withdrawan successfully")
+            if amount <= self.__balance:
+                self.__balance -= amount
+                print("Withdrawal successfully")
             else:
-                print("insufficiant amount")
+                print("insufficient amount")
         else:
             print("Invalid Pin")
     
     def check_balance(self):
         temp = input("Enter your pin :")
-        if temp == self.pin:
-            print(self.balance)
+        if temp == self.__pin:
+            print(self.__balance)
         else:
             print("Inavalid pin")
         
-
-
+sbi = Atm()
 
 
 
